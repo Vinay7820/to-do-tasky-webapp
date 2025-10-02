07c4a82dcf019db381e1137fe48657cf13dc38e4
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-10-02 23:31:06 +0530

    Update ecr.tf
    added pushing image local-exec block

--- terraform-scripts/ecr.tf
+++ terraform-scripts/ecr.tf
@@ -1,3 +1,15 @@
 resource "aws_ecr_repository" "this" {
   name = "${var.project}-tasky"
 }
+
+resource "null_resource" "push_image" {
+  depends_on = [
+    aws_ecr_repository.tasky,
+    aws_eks_cluster.this
+  ]
+
+  provisioner "local-exec" {
+    command     = "./push.sh"
+    working_dir = "${path.module}"   # ensures it runs from terraform-scripts/
+  }
+}
