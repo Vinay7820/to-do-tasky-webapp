f4ec079fe842638bc0db62e69d9f0ef3144aeac1
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-10-03 01:13:50 +0530

    Update push.sh
    Fixed Multi-Arch Issue

--- terraform-scripts/push.sh
+++ terraform-scripts/push.sh
@@ -8,8 +8,16 @@ TAG="${1:-wiz-v1}"
 
 aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "$REPO_URI"
 
-docker build -t tasky-app ../tasky-app
-docker tag tasky-app:latest "$REPO_URI:$TAG"
-docker push "$REPO_URI:$TAG"
+#docker build -t tasky-app ../tasky-app
+#docker tag tasky-app:latest "$REPO_URI:$TAG"
+#docker push "$REPO_URI:$TAG"
 
+docker buildx create --use || true
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t "$REPO_URI:$TAG" \
+  ../tasky-app \
+  --push \
+  --provenance=false
+  
 kubectl rollout restart deployment tasky -n tasky-wiz
