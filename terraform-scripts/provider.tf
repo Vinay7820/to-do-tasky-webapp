b795dfea09c675f4ca4cb8c934935f69fb5ca2b4
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-10-04 19:18:46 +0530

    Update provider.tf

--- terraform-scripts/provider.tf
+++ terraform-scripts/provider.tf
@@ -17,5 +17,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
+  depends on             = [aws_eks_cluster.this]
 }
 
