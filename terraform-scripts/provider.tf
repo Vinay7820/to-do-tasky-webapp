ad06855a0f41cc34bd3701445a5d695146ccd81a
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-10-04 19:05:46 +0530

    Update provider.tf

--- terraform-scripts/provider.tf
+++ terraform-scripts/provider.tf
@@ -5,11 +5,11 @@ provider "aws" {
 
 # EKS cluster data sources
 data "aws_eks_cluster" "this" {
-  name = "aws_eks_cluster.this.name"
+  name = "aws_eks_cluster.this"
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = "aws_eks_cluster_auth.this.name"
+  name = "aws_eks_cluster_auth.this"
 }
 
 # Kubernetes provider to interact with EKS
