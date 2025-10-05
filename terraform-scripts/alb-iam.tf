8f7c7f573aaf1be128e3fa58eab9ce047a5b4cad
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-10-05 14:47:23 +0530

    Update alb-iam.tf

--- terraform-scripts/alb-iam.tf
+++ terraform-scripts/alb-iam.tf
@@ -1,5 +1,27 @@
+#########################################
+# ALB Controller IAM + OIDC Integration #
+#########################################
+
+# Fetch EKS cluster details
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+# Fetch authentication info for cluster
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+# Create an IAM OIDC provider for EKS
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.aws_eks_cluster.this.certificate_authority[0].data]
+  url             = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+# Create IAM role for ALB controller
 resource "aws_iam_role" "alb_controller" {
-  name = "tasky-wiz-alb-controller-role"
+  name = "AWSLoadBalancerControllerRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -20,7 +42,16 @@ resource "aws_iam_role" "alb_controller" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
+# Create the ALB Controller IAM Policy
+resource "aws_iam_policy" "alb_controller" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "Policy for ALB Controller to manage AWS resources"
+
+  policy = file("${path.module}/policies/AWSLoadBalancerControllerIAMPolicy.json")
+}
+
+# Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "alb_controller" {
   role       = aws_iam_role.alb_controller.name
-  policy_arn = "arn:aws:iam::150575195000:policy/AWSLoadBalancerControllerIAMPolicy"
+  policy_arn = aws_iam_policy.alb_controller.arn
 }
