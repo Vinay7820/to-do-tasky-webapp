eb8b370dead829e6544cde6a49de94f6ac7fe355
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-10-05 14:45:37 +0530

    Update alb-sa.tf

--- terraform-scripts/alb-sa.tf
+++ terraform-scripts/alb-sa.tf
@@ -1,13 +1,19 @@
 resource "kubernetes_service_account" "alb_controller" {
+  provider = kubernetes.eks
+
   metadata {
     name      = "aws-load-balancer-controller"
     namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller.arn
+    labels = {
+      "app.kubernetes.io/name" = "aws-load-balancer-controller"
     }
   }
 
   depends_on = [
-    aws_iam_role.alb_controller
+    aws_iam_role.alb_controller,
+    aws_iam_openid_connect_provider.eks,
+    aws_eks_cluster.this,
+    aws_eks_node_group.this,
+    null_resource.update_kubeconfig
   ]
 }
