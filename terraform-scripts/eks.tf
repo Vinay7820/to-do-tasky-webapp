1540b5a7fbe75f7348c68dbbdc3395c4cd873aaa
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2025-10-05 18:47:54 +0530

    Update eks.tf

--- terraform-scripts/eks.tf
+++ terraform-scripts/eks.tf
@@ -7,7 +7,7 @@ resource "aws_eks_cluster" "this" {
     subnet_ids             = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
     # endpoint_public_access = true
     endpoint_private_access = true
-    endpoint_public_access  = false
+    endpoint_public_access  = true
   }
 }
 
